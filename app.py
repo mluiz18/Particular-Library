e4f2559dfb9094510eceac24e4295338755afb87
Miguel Luiz Amaral da Silva Barbosa <migueluiz18@gmail.com>
2025-12-04 20:46:26 -0300

    feat: add first flask route

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
+from flask import Flask, request, url_for, session, flash, redirect
 from models.Book import Book
+
 
 def get_html(content, title="Library"):
     return f'''
-            <!DOCTYPE html>
+        <!DOCTYPE html>
         <html lang="pt-br">
         <head>
             <meta charset="UTF-8">
@@ -14,3 +16,19 @@ def get_html(content, title="Library"):
         </body>
         </html>
     '''
+
+
+b1 = Book()
+b1.setName("1984")
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=["POST", "GET"])
+def inital_page():
+    c = f"{b1.getName()}"
+    return get_html(c)
+
+if __name__ == "__main__":
+    print("http://localhost:5000")
+    app.run(debug=True, host='0.0.0.0', port=5000)
